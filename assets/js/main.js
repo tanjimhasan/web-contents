951861429640dcd225ed8c4fa5ffa2c4901fc394
tanjimhasan <tanjim.hasan.pro@gmail.com>
2018-10-29 01:30:45 +0600

    input area focus problem removed

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
         }
         //pagination
         $('#section-container').paginate({
-            'perPage': 16
+            'perPage': 8
         });
     });
 	
